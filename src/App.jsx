e5d14b868663d2de82d7d50b04e22a81172a5199
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-08-27 02:02:46 -0700

    multiples components being called

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ function App() {
   return (
  <>
  <Card name={"abc"} age={123} />
+ <Card name={"ayx"} age={122} />
+ <Card name={"agf"} age={121243} />
+ <Card name={"xyz"} age={342} />
  </>
   )
 }
